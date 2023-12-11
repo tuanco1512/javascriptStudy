4e107bce2504ccaa039fb7f8665025782875cd7a
tuanco1512 <tuanco15122000@gmail.com>
2023-12-11 19:13:52 +0700

    a

--- index.js
+++ index.js
@@ -1726,15 +1726,19 @@
 
 // show/hide HTML elements
 
-const myButton = document.querySelector("#myButon");
-const myImg = document.querySelector("#myImg");
+// const myButton = document.querySelector("#myButton");
+// const myImg = document.querySelector("#myImg");
 
-myButton.addEventListener("click", () => {
+// myButton.addEventListener("click", () => {
+//     console.log(myImg.style.display);
+//     if(myImg.style.visibility == "hidden"){
+//         myImg.style.visibility = "visible";
+//     }
+//     else{
+//         myImg.style.visibility = "hidden";
+//     }
+// })
 
-    if(myImg.style.display == "none"){
-        myImg.style.display = "block";
-    }
-    else{
-        myImg.style.display = "none";
-    }
-})
+
+
+// detect key presses
